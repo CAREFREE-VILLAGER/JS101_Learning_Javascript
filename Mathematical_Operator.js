e6e3e20705e94fc545c4169e8647b1c2321073bf
CAREFREE-VILLAGER <as314202@gmail.com>
2023-08-30 14:37:37 +0530

    added mathematical operator code

---
+++ Mathematical_Operator.js
@@ -0,0 +1,191 @@
+// There are some rules for creating variable name
+
+//1. the variable name cannot start with a number
+// let 12 = "hello"
+
+// 2. a variable should not start with a symbol
+
+// let @name = "Tanya"
+
+// $, _
+
+// let $adharcard = 12345;
+
+// let _name = "Tanya";
+
+// The name should be self explanatory
+
+//1. way
+
+// let name1 = "Abhishek"
+// let name2 =  "Aishwarya"
+// let name3  = "Aaradhya"
+
+//2. way
+
+// let fatherName = "Abhishek";
+// let motherName = "Aishwarya";
+// let daughterName = "Aaradhya";
+
+// camel case
+
+// let fatherName = "Abhishek";
+// let motherName = "Aishwarya";
+// let daughterName = "Aaradhya";
+// let myAdharCard = "123456"
+
+// snake case
+
+let father_name = "Abhishek";
+let mother_name = "Aishwarya";
+
+prob1.js
+--------------------------------------------------------------------
+// let a = 6;
+// let b = 2;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b)
+
+// console.log(5%2);
+// console.log(2 % 5)
+
+// console.log(5**2)
+// console.log(9**0.5)
+
+// let a = "6"
+// let b =  6
+// let sum = a + b
+
+// console.log(typeof(sum))
+
+// console.log("Hello" + " " + "world")
+
+console.log(10 / "2")
+
+quiz1.js
+-------------------------------------------------------------------------------------
+// What is the result of 5 + 3?
+// a) 2
+// b) 7
+// c) 8
+// d) 15
+
+// Evaluate the expression 10 - 4 * 2.
+// a) 2
+// b) 4
+// c) 6
+// d) 12
+
+// Calculate the value of 15 / 3.
+// a) 3
+// b) 5
+// c) 10
+// d) 15
+
+// Find the remainder when 27 is divided by 4 using the modulo operator.
+// a) 1
+// b) 2
+// c) 3
+// d) 4
+
+// What is the result of 2 ** 4? (Exponentiation)
+// a) 2
+// b) 4
+// c) 8
+// d) 16
+// Evaluate the expression: (2 + 5) * 3 - 4 / 2
+// a) 19
+// b) 18
+// c) 16
+// d) 17
+
+
+quiz2.js
+-----------------------------------------------------------------------------------------
+// relational operator
+
+
+// Guess the output
+
+
+console.log(5 > 8);
+console.log(5 >= 7);
+console.log(6 < 7);
+console.log(5 <= 5)
+
+console.log(5 == 5);
+
+console.log(5 === 5);
+
+console.log(6 == "6");
+
+console.log(6 === "6");
+
+console.log(1 == true);
+
+console.log(1 === true);
+
+console.log(6 != 7);
+
+console.log(6 != 6);
+
+console.log(6 != "6");
+
+console.log(6 !== 7)
+
+console.log(6 !== 6);
+
+console.log(6 !== "6");
+
+quiz3.js
+---------------------------------------------------------------------------------------
+
+// Given the variables firstName = 'John' and lastName = 'Doe', what is the result of firstName + lastName?
+// a) "JohnDoe"
+// b) "John Doe"
+// c) "DoeJohn"
+// d) "Doe John"
+
+// What is the output of the following code snippet?
+
+// javascript
+// // Copy code
+// let greeting = 'Hello';
+// let name = 'Alice';
+// let message = greeting + ', ' + name + '!';
+
+// console.log(message);
+// a) "Hello"
+// b) "Alice"
+// c) "Hello, Alice!"
+// d) "Hello name!"
+
+// Given the variables num1 = 10 and num2 = 5, what is the result of num1 + num2 + ' apples'?
+// a) "15"
+// b) "10 apples"
+// c) "15apples"
+// d) "105 apples"
+
+// What is the value of result after executing the following code?
+
+// let result = 'I have ' + 3 + 4 + ' apples.';
+// a) "I have 7 apples."
+// b) "I have 34 apples."
+// c) "I have 3 apples."
+// d) "I have 4 apples."
+
+// What is the output of the following code snippet?
+
+// javascript
+// Copy code
+// let x = 10;
+// let y = 5;
+// console.log('The sum of ' + x + ' and ' + y + ' is ' + (x + y) + '.');
+// a) "The sum of 10 and 5 is 15."
+// b) "The sum of x and y is 10 + 5."
+// c) "The sum of x and y is 15."
+// d) "The sum of 10 and 5 is xy."
+
